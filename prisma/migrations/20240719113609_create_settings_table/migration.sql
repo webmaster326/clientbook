33f9557281a3d964bce97b56b936c108e94899ad
anjali <anjali.dakshadesign@gmail.com>
2024-08-06 14:38:16 +0530

    delete migrations

--- prisma/migrations/20240719113609_create_settings_table/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Settings" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "clientid" TEXT NOT NULL,
-    "clientsecret" TEXT NOT NULL
-);
--- prisma/migrations/20240730100655_create_jewelryform_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "JewelryDesignForm_emailAddress_key";
